2f577004e3675bee14a00d65d213f804ae565287
Felipefams <augustofams@gmail.com>
2022-05-11 23:06:15 -0300

    re-doing E

---
+++ codeforces/contests/1669div4/e2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+#define sqr(a) a*a
+#define ll long long
+#define ld long double
+#define sz(x) ((int)x.size())
+
+typedef pair<int,int> pair_int;
+//se tiver um numero impar de B's ou R's e em seguida um W, nao vai dar pra pllar, a menos que o W esteja na ultima posicao;
+void solve(int k){
+	int n = 0;
+	cin >> n;
+	/*vector<string> z; 
+	for(int i = 0; i < n; ++i){
+		cout << z;
+	}
+	*/
+}
+
+int main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+    cout.tie(0);
+	int t = 0;
+/*	int x = 'k' - 'a';
+	cout << (int) 'a' << '\n';
+*/
+	cin >> t;
+	while(t--){
+
+	}
+	
+	return (0);
+}
