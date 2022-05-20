4f729e7f2019ee976796d9d37ff608adec2ffa54
Felipefams <augustofams@gmail.com>
2022-05-19 23:14:25 -0300

    permutations done

--- cses_pset/permutations.cpp
+++ cses_pset/permutations.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 #define PI 3.1415926535897932384626433832795
 #define sqr(a) a*a
+#define pb push_back
+#define eb emplace_back
 #define ll long long
 #define ld long double
 #define sz(x) ((int)x.size())
@@ -13,16 +15,35 @@ using namespace std;
 typedef pair<int,int> pair_int;
 
 void solve(){
-	//solution
+	ll n; cin >> n;
+	if(n < 5){
+		if(n == 1)
+			cout << 1; 
+		else if(n == 4)
+			cout << "2 4 1 3";
+		else
+		cout << "NO SOLUTION";
+	}else{
+		vector<ll> even(n/2 + 1);
+		vector<ll> odd(n/2 + 1);
+		for(ll i = 1; i <= n; ++i){
+			if(i & 1) odd.eb(i);
+			else even.eb(i);
+		}
+		for(auto &x : even){
+			if(x != 0)
+				cout << x << " ";
+		}
+		for(auto &x : odd){
+			if(x != 0)
+				cout << x << " ";
+		}
+	}
 }
 
 int main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
-	int t = 0;
-	cin >> t;
-	while(t--){
-
-	}
+	solve();
 	return (0);
 }
