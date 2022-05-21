690bf3cd031329a879f169beda554681ade88ea4
Felipefams <augustofams@gmail.com>
2022-05-21 17:49:15 -0300

    .

--- codeforces/800/template.cpp
+++ codeforces/800/template.cpp
@@ -26,7 +26,6 @@ typedef vector<int> vi;
 typedef pair<int,int> pii;
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
 
-
 void solve(){
 	//solution
 }
