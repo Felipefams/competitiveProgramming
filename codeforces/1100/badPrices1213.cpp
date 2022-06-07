48bf0da3acd67f19271b9ce47b33ed36c656b5a2
Felipefams <augustofams@gmail.com>
2022-06-07 17:55:31 -0300

    done

--- codeforces/1100/badPrices1213.cpp
+++ codeforces/1100/badPrices1213.cpp
@@ -10,8 +10,8 @@ using namespace std;
 #define sqr(a) (a)*(a)
 #define sz(x) ((int)x.size())
 #define watch(x) cout << (#x) << " is " << (x) << endl
-#define max3(a, b, c) max(a, max(b, c)) 
-#define min3(a, b, c) min(a, min(b, c)) 
+//#define max3(a, b, c) max(a, max(b, c)) we can just do max({e1,e2,e3});
+//#define min3(a, b, c) min(a, min(b, c)) same goes for here min({e1,e2,e3});
 //utils
 #define umap unordered_map
 #define uset unordered_set
@@ -26,42 +26,17 @@ typedef vector<int> vi;
 typedef pair<int,int> pii;
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
 
-bool binarySearch(vector<ll> &v, ll x){
-	int l = 0; int r = v.size() - 1;
-	while(l <= r){
-		int m = l + (r-1) / 2;
-		if(arr[m] > x)
-			return true;
-		if(arr[m] < x)
-			l = m + 1;
-		else 
-			r = m - 1;
-	}
-	return false;
-}
-
 void solve(){
 	int n; cin >> n;
 	int count = 0;
 	vector<ll> v(n);
 	for(int i = 0; i < n; ++i) cin >> v[i];	
-	for(auto x : v) cout << x;
-	cout << endl;
-	sort(v.begin(), v.end());
-	for(auto x : v){
-
+	ll m = imax;
+	for(int i = n-1; i >= 0; --i){
+		if(v[i] > m)
+			count++;
+		m = min(v[i], m);
 	}
-	/*
-	for(int i = 0; i < n; ++i){
-		for(int k = i; k < n; ++k){
-			if(v[k] < v[i]){
-				count++;
-				break;
-			}
-		}
-	}
-	*/
-
 	cout << count << "\n";
 }
 
