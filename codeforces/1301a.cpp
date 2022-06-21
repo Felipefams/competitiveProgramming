80698fba757effc621c7083deb6539351daa97b6
Felipefams <augustofams@gmail.com>
2022-06-21 20:50:49 -0300

    start

---
+++ codeforces/1301a.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//constants
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+//functions
+#define sqr(a) (a)*(a)
+#define sz(x) ((int)x.size())
+#define watch(x) cout << (#x) << " is " << (x) << endl
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+
+
+bool solve(){
+	string a,b,c;
+	cin >> a >> b >> c;
+	if(c == a || c == b) return true;
+
+}
+
+// cout << "Case #" << t << ": ";
+int main(){
+	fast_io;
+	int t = 0;
+	cin >> t;
+	while(t--){
+
+	}
+	return (0);
+}
