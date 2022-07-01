a346c803275dc9bc023217f81c880ff7c998d6a0
Felipefams <augustofams@gmail.com>
2022-07-01 17:14:37 -0300

    fim praga

--- b.cpp
+++ b.cpp
@@ -1,3 +1,4 @@
+//https://codeforces.com/contest/1339/problem/B
 #include <bits/stdc++.h>
 using namespace std;
 //vrum vrum
@@ -30,33 +31,16 @@ bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) retu
 
 void solve(){
 	int n; cin >> n;
-	vector<ll> v;	
-	for(int i = 0; i < n; ++i){
-		int k; cin >> k;
-		v.pb(k);
-	}
-	vector<pair<ll, ll>> pv; 
-	for(int i = 1; i < n; i += 2){
-		pair<ll, ll> tmp = {v[i-1], v[i]};
-		pv.pb(tmp);
-	}
-	vector<ll> diff;
-	for(auto &x : pv){
-		diff.pb(x.fi - x.sc);
-	}
-	vector<ll> sdiff = diff;
-	sort(sdiff.begin(), sdiff.end());
-	umap<ll,int> newPos;
-	vector<int> pos;
-	for(int i = 0; i < sdiff.size(); ++i){
-		newPos[sdiff[i]] = i;
-	}
-	vector<pair<ll, ll>> tmp = pv;
-	for(int i = 0; i < n/2; ++i){
-		tmp[i] = pv[newPos[diff[i]]];
-	}
-	for(int i = 0; i < n/2; ++i){
-		cout << tmp[i].fi << " " << tmp[i].sc << " ";
+	vector<ll> v(n);	
+	for(int i = 0; i < n; ++i) cin >> v[i];
+	sort(v.begin(), v.end());
+	for(int i = (n-1)/2; i >= 0; --i){
+		if((n-1) % 2 == 0 && i == (n-1)/2){
+			cout << v[i] << " ";
+		}
+		else{
+			cout << v[i] << " " << v[n-1-i] << " ";
+		}
 	}
 	cout << endl;
 }
@@ -71,3 +55,47 @@ int main(){
 	}
 	return (0);
 }
+/* old solution,
+void solve(){
+	int n; cin >> n;
+vector<ll> v;	
+for(int i = 0; i < n; ++i){
+	int k; cin >> k;
+	v.pb(k);
+}
+vector<pair<ll, ll>> pv; 
+for(int i = 1; i < n; i += 2){
+	pair<ll, ll> tmp = {v[i-1], v[i]};
+	pv.pb(tmp);
+}
+vector<ll> diff;
+for(auto &x : pv){
+	diff.pb(llabs(x.fi - x.sc));
+}	
+vector<ll> sdiff = diff;
+sort(sdiff.begin(), sdiff.end());
+umap<ll, int> mp;
+for(int i = 0; i < sdiff.size(); ++i){
+	mp[sdiff[i]] = i;
+}
+vector<pair<ll,ll>> ans(pv.size());
+for(int i = 0; i < diff.size(); ++i){
+	ans[mp[diff[i]]] = pv[i];
+	watch(mp[diff[i]]);
+	watch(pv[mp[diff[i]]].fi);
+	watch(pv[mp[diff[i]]].sc);
+}
+for(auto &x : diff){
+	cout << x << " ";
+}
+cout << endl;
+for(auto &x : sdiff){
+	cout << x << " ";
+}
+cout << endl;
+for(auto &x : ans){
+	cout << x.fi << " " << x.sc << " ";
+}
+cout << endl;
+}
+*/
