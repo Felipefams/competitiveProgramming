18975724bcf8059d084f325b207bc83b391f5d80
Felipefams <augustofams@gmail.com>
2022-07-15 17:48:02 -0300

    fatorial

---
+++ factorial.cpp
@@ -0,0 +1,46 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1936?origem=1
+#include <bits/stdc++.h>
+using namespace std;
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//constants
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+//functions
+#define sqr(a) (a)*(a)
+#define sz(x) ((int)x.size())
+#define watch(x) cout << (#x) << " is " << (x) << endl
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+
+// cout << "Case #" << t << ": ";
+int main(){
+	fast_io;	
+	int n; cin >> n;
+	vector<int> fat(11);
+	fat[0] = 1;
+	for(int i = 1; i < 11; ++i) fat[i] = fat[i-1] * i;	
+	int ans = 0;
+	for(int i = 10; i > 0; --i){
+		int at = n/fat[i];
+		ans += at;
+		n -= at*fat[i];
+	}
+	cout << ans << endl;
+	return (0);
+}
