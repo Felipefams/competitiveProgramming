53074ab5472d3d68f35fb50a03a4d825421106f6
Felipefams <augustofams@gmail.com>
2022-07-22 20:18:11 -0300

    dragon quest

---
+++ codeforces/900/1337b.cpp
@@ -0,0 +1,55 @@
+//https://codeforces.com/contest/1337/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+//constants
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+//functions
+#define sqr(a) (a)*(a)
+#define sz(x) ((int)x.size())
+#define watch(x) cout << (#x) << " is " << (x) << endl
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+
+
+void solve(){
+	int x,n,m; cin >> x >> n >> m;
+	if(x > 10){
+		while(n--){
+			x = x/2 + 10;
+		}
+	}
+	x = x - m*10;
+	if(x > 0) cout << "NO\n";
+	else cout << "YES\n";
+}
+
+// cout << "Case #" << t << ": ";
+int main(){
+	fast_io;
+	int t = 0;
+	cin >> t;
+	while(t--){
+		solve();
+	}
+	return (0);
+}
