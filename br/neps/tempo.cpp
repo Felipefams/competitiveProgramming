a0f64747ea13e9ee3006c5902ed3be676b7c7223
Felipefams <augustofams@gmail.com>
2022-07-27 21:38:16 -0300

    tempo

--- br/neps/tempo.cpp
+++ br/neps/tempo.cpp
@@ -1,41 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//constants
 #define PI 3.1415926535897932384626433832795
-#define sqr(a) a*a
-#define ll long long
-#define ld long double
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+//functions
+#define sqr(a) (a)*(a)
 #define sz(x) ((int)x.size())
 #define watch(x) cout << (#x) << " is " << (x) << endl
-#define max3(a, b, c) max(a, max(b, c)) 
-#define min3(a, b, c) min(a, min(b, c)) 
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
 
-typedef pair<int,int> pair_int;
+int vt[101];
+vector<bool> emEspera(101, false);
 
-void solve(){
-	cout << "looooser\n";
+void increment(int n){
+	for(int i = 0; i < 101; ++i){	
+		if(emEspera[i])
+			vt[i] += n;
+	}
 }
 
-//falta terminar
-int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int t = 0;
-	cin >> t;
-	vector<int> arr;
-	list<int> l;
-	int count = 0;
-	while(t--){
+void solve(){
+	int n; cin >> n;
+	umap<int,int> countMsg;
+	for(int i = 0; i < n; ++i){
 		char c; cin >> c;
-		string s; cin >> s;
-		const int svalue = stoi(s);
-		if(c == 'E')
-			cout << svalue << " " << arr[svalue] << '\n';	
-		else if(c == 'T')
-			count += svalue;
-		count++;
-
+		int t; cin >> t;
+		increment(1);
+		if(c == 'W'){
+			increment(t - 2);
+		}	
+		else if(c == 'R'){
+			emEspera[t] = true;
+			countMsg[t]++;
+		}
+		else if(c == 'S'){
+			emEspera[t] = false;
+			countMsg[t]--;
+		}
+//		watch(vt[t]);
 	}
+	for(int i = 0; i < 101; ++i){
+		if(countMsg[i] != 0)
+			vt[i] = -1;
+		if(vt[i] != 0)
+			cout << i << " " << vt[i] << endl;
+	}
+}
 
+// cout << "Case #" << t << ": ";
+int main(){
+	fast_io;
+	solve();
 	return (0);
 }
--- br/obi/tempo.cpp
+++ br/obi/tempo.cpp
@@ -26,18 +26,41 @@ typedef vector<int> vi;
 typedef pair<int,int> pii;
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
 
+int vt[101];
+vector<bool> emEspera(101, false);
+
+void increment(int n){
+	for(int i = 0; i < 101; ++i){	
+		if(emEspera[i])
+			vt[i] += n;
+	}
+}
 
 void solve(){
-	int t = 0;
-	cin >> t;
-	set<pii> sp;
-	queue<pair<char,int>> q;
-	while(t--){
-		char c; cin >> c;	
-		int k; cin >> k;
-		pair<char,int> tmp;
-		tmp.fi = c; tmp.sc = k;
-		q.emplace(tmp);
+	int n; cin >> n;
+	umap<int,int> countMsg;
+	for(int i = 0; i < n; ++i){
+		char c; cin >> c;
+		int t; cin >> t;
+		increment(1);
+		if(c == 'W'){
+			increment(t - 2);
+		}	
+		else if(c == 'R'){
+			emEspera[t] = true;
+			countMsg[t]++;
+		}
+		else if(c == 'S'){
+			emEspera[t] = false;
+			countMsg[t]--;
+		}
+//		watch(vt[t]);
+	}
+	for(int i = 0; i < 101; ++i){
+		if(countMsg[i] != 0)
+			vt[i] = -1;
+		if(vt[i] != 0)
+			cout << i << " " << vt[i] << endl;
 	}
 }
 
