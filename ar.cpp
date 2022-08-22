0dfe01dec979cf16167cfd8398bf7f3b7efb35bf
Felipefams <augustofams@gmail.com>
2022-08-21 23:05:50 -0300

    .

---
+++ ar.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+//constants
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+//functions
+#define sqr(a) (a)*(a)
+#define sz(x) ((int)x.size())
+#define watch(x) cout << (#x) << " is " << (x) << endl
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+
+
+void solve(){
+	int n; ll b; cin >> n >> b;
+	vector<ll> v(n);
+	for(int i = 0; i < n; ++i) cin >> v[i];
+	sort(v.begin(), v.end());	
+	int i = 0, j = n-1;
+	int count = 0;
+	while(i <= j){
+		if(v[i] == b){
+
+		}
+	}
+}
+
+// cout << "Case #" << t << ": ";
+int main(){
+	fast_io;
+	
+	return (0);
+}
