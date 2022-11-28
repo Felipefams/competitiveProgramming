7fd9e76888d0c4b545020621ddde5747c715df21
Felipefams <augustofams@gmail.com>
2022-11-28 00:17:03 -0300

    .

---
+++ usaco/bronze/maximumDistance.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//constants
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+//functions
+#define sqr(a) (a)*(a)
+#define sz(x) ((int)x.size())
+#define watch(x) cout << (#x) << " is " << (x) << endl
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+template<typename T>
+std::ostream & operator << (std::ostream &os ,std::vector<T> &v){
+	for(auto& x : v) os << x << " ";	
+	os << endl;
+	return os;
+}
+
+void solve(){
+	int n; cin >> n;
+	vector<int> a(n), b(n);
+	for(int i = 0; i < n; ++i) cin >> a[i];
+	for(int i = 0; i < n; ++i) cin >> b[i];	
+	vector<int> ans;
+	for(int i = 0; i < n; ++i){
+		for(int j = i+1; j < n; ++j){
+			int dx = a[i] - a[j], dy = b[i] - b[j];
+			int square = dx*dx + dy*dy;
+			ans.pb(square);
+		}
+	}
+	int maior = ans[0];
+	for(int i = 0; i < ans.size(); ++i){
+		if(ans[i] > maior){
+			maior = ans[i];
+		}
+	}
+	cout << maior << endl;
+}
+
+// cout << "Case #" << t << ": ";
+int main(){
+	fast_io;
+	solve();
+
+	return (0);
+}
