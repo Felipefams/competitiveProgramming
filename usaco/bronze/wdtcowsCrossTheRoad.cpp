fa785c4498f7c9dea374b31b0f5ff15a6c8d7844
Felipefams <augustofams@gmail.com>
2022-12-03 11:50:58 -0300

    done

--- usaco/bronze/wdtcowsCrossTheRoad.cpp
+++ usaco/bronze/wdtcowsCrossTheRoad.cpp
@@ -32,15 +32,35 @@ std::ostream & operator << (std::ostream &os ,std::vector<T> &v){
 	os << endl;
 	return os;
 }
-
+bool b[26][26];
+bool c[26];
 void solve(){
-	//solution
+    ifstream fin ("circlecross.in");
+	ofstream fout ("circlecross.out");	
+	string s; fin >> s;
+	const int n = 52;
+	int ans = 0;
+	for(int i = 0; i < n; ++i){
+		vector<int> dp(26);
+		if(c[s[i] - 'A']) continue;
+		for(int j = i+1; s[j] != s[i] && j < n; ++j){
+//			if(b[s[i]- 'A'][s[j] - 'A'])
+			dp[s[j] - 'A']++;
+		}
+		for(int j = 0; j < dp.size(); ++j){
+			if(dp[j] == 1 && !b[s[i]-'A'][j] && !b[j][s[i]-'A']){
+				b[s[i]-'A'][j] = 1;
+				b[j][s[i]-'A'] = 1;
+				ans++;
+			}
+		}	
+		c[s[i] - 'A'] = 1;
+	}
+	fout << ans;
 }
 
 // cout << "Case #" << t << ": ";
 int main(){fast_io;
-    ifstream fin ("test.in");
-	ofstream fout ("test.out");	
-
+	solve();
 	return (0);
 }
