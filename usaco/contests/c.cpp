c6756c9588cf312089c2dd8224407e13eeae62ae
Felipefams <augustofams@gmail.com>
2022-12-21 18:03:11 -0300

    first contest rip

---
+++ usaco/contests/c.cpp
@@ -0,0 +1,57 @@
+/*
+ID: augusto15
+TASK: test
+LANG: C++                 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//constants
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+//functions
+#define sqr(a) (a)*(a)
+#define sz(x) ((int)x.size())
+#define watch(x) cout << (#x) << " is " << (x) << endl
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+template<typename T>
+std::ostream & operator << (std::ostream &os ,std::vector<T> &v){
+	for(auto& x : v) os << x << " ";	
+	os << endl;
+	return os;
+}
+ifstream fin ("test.in");
+ofstream fout ("test.out");	
+
+void solve(){
+	int n, m; cin >> n >> m;
+
+}
+
+// cout << "Case #" << t << ": ";
+int main(){fast_io;
+	int t; cin >> t;
+	while(t--){
+		solve();
+	}
+	return (0);
+}
+
+
