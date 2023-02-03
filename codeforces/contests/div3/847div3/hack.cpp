88a8b748ef094e9c8b757e9bb18e66927fe65b92
Felipefams <augustofams@gmail.com>
2023-02-03 15:07:44 -0300

    div3 folders organized

---
+++ codeforces/contests/div3/847div3/hack.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+//vrum vrum
+#define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
+//constants
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define exp 1e9
+#define imax INT_MAX
+#define imin INT_MIN
+//functions
+#define sqr(a) (a)*(a)
+#define sz(x) ((int)x.size())
+#define watch(x) cout << (#x) << " is " << (x) << endl
+//utils
+#define umap unordered_map
+#define uset unordered_set
+#define pb push_back
+#define mk make_pair
+#define fi first
+#define sc second
+//types
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
+template<typename T>
+std::ostream & operator << (std::ostream &os ,std::vector<T> &v){
+	for(auto& x : v) os << x << " ";	
+	os << endl;
+	return os;
+}
+
+void solve(){
+	//solution
+}
+ofstream fout ("hack.out");	
+
+// cout << "Case #" << t << ": ";
+int main(){
+	fast_io;
+	ll k = 1;
+	fout << k << endl;
+	const int mv = 2e5;
+	int i = 1;
+	ll x = 107897;
+	while(k--){
+		int tmp = mv;
+		fout << mv << endl;
+		while(tmp--){
+			if(x*i >= 1e9) i = 1;
+			fout << x*i << " ";
+			++i;
+		}
+		fout << endl;	
+	}
+	return (0);
+}
+
