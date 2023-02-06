0fa860f0abad6ab76df9a49e2bd8b7eb0a4e3253
Felipefams <augustofams@gmail.com>
2023-02-05 21:55:37 -0300

    solved 849d

---
+++ codeforces/d.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+#define ll long long
+
+#define          all(v)              v.begin(), v.end()
+#define         rall(v)              v.rbegin(),v.rend()
+ 
+#define            pb                push_back
+#define          sz(a)               (int)a.size()
+
+void solve() {
+    int n; string s; cin >> n >> s;
+    vector<int> cnt(26, 0), p(26, 0);
+    for(auto x: s) cnt[x - 'a']++;
+    int ans = 0;
+    for(auto x: s) {
+        --cnt[x - 'a'];
+        ++p[x - 'a'];
+        int cur = 0;
+        for(int i = 0; i < 26; ++i) {
+            cur += min(1, cnt[i]) + min(1, p[i]);
+        }
+        ans = max(ans, cur);
+    }
+    cout << ans << "\n";
+}   
+ 
+int32_t main() {
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+}
