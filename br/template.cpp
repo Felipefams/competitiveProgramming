2154a9d5a5dc3bedda631bf8cd5796e005aa0684
Felipefams <augustofams@gmail.com>
2023-02-19 23:10:06 -0300

    template

--- br/template.cpp
+++ br/template.cpp
@@ -3,6 +3,7 @@ using namespace std;
 //vrum vrum
 #define fast_io ios_base::sync_with_stdio(0); cin.tie(0);
 //constants
+#define endl "\n"
 #define PI 3.1415926535897932384626433832795
 #define MOD 1000000007
 #define exp 1e9
@@ -25,7 +26,12 @@ typedef long double ld;
 typedef vector<int> vi;
 typedef pair<int,int> pii;
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i*i<=a;++i) if (a%i==0) return 0; return 1; }
-
+template<typename T>
+std::ostream & operator << (std::ostream &os ,std::vector<T> &v){
+	for(auto& x : v) os << x << " ";	
+	os << endl;
+	return os;
+}
 
 void solve(){
 	//solution
