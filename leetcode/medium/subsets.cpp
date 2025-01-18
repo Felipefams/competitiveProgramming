c48a84a8f913109932ff2fdb01b69711b4e4df1b
Felipefams <86446935+Felipefams@users.noreply.github.com>
2025-01-18 20:12:19 -0300

    chore(lint): format text

--- leetcode/medium/subsets.cpp
+++ leetcode/medium/subsets.cpp
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> v;
-        vector<vector<int>> ans;
-        solve(0, v, nums, ans);
+vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+    vector<int> v;
+    vector<vector<int>> ans;
+    solve(0, v, nums, ans);
 
-        sort(ans.begin(), ans.end());
+    sort(ans.begin(), ans.end());
 
-        return ans;
+    return ans;
+}
+
+void solve(int i, vector<int> &v, vector<int> &nums, vector<vector<int>> &ans) {
+    if (i >= nums.size()) {
+        return;
     }
-
-    void solve(int i, vector<int> &v, vector<int> &nums, vector<vector<int>> &ans){
-        
-        if(i >= nums.size()){
-            return;
-        }
-        v.push_back(nums[i]);
-        solve(i+1, v, nums, ans);
-        ans.push_back(v);
-        v.pop_back();
-        solve(i+1, v, nums, ans);
-    }
+    v.push_back(nums[i]);
+    solve(i + 1, v, nums, ans);
+    ans.push_back(v);
+    v.pop_back();
+    solve(i + 1, v, nums, ans);
+}
 
 int main(void) {
     vector<int> nums = {1, 2, 3};
